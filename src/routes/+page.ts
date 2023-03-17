79481287cff8777ecf01d6f16bd46b752232ee00
Pat Sissons <patricksissons@gmail.com>
2023-03-17 16:30:49 -0700

    fixing bad import path

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit'
-import type { PageLoad } from './movies/$types'
+import type { PageLoad } from './$types'
 
 export const load = (() => {
   throw redirect(302, '/movies/search')
